bafb30ea089cbd7af6e3dc21af7d238a176b03d2
Joseph Madigan <jmad1112x@gmail.com>
2024-12-03 23:02:10 -0800

    Initial BE-Prototype

--- proto/models.py
+++ proto/models.py
@@ -1,7 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, ConfigDict
 
 class Email(BaseModel):
     id: int
-    from_: str
+    from_: str = Field(alias='from')
     subject: str
     body: str
+
+    model_config = ConfigDict(populate_by_name=True)
+
+
+class EmailSummary(BaseModel):
+    email_id: int
+    summary: str
