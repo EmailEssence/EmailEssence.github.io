4298c762969af85ace77574628fdfc67fd589373
mads <mads.flac@proton.me>
2025-01-08 12:56:25 -0800

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,9 +7,12 @@ from backend.app.routes.summaries_routes import router as summaries_router
 
 from backend.app.models.email_model import Email
 from backend.app.models.summary_model import EmailSummary
+from backend.app.models.user_model import User
 
 app = FastAPI()
 
 # Register routers
 app.include_router(emails_router, prefix="/emails", tags=["Emails"])
 app.include_router(summaries_router, prefix="/summaries", tags=["Summaries"])
+app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+
