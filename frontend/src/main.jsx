d15090978ea11741528d8d2c08751d54ff425c7d
emmamelkumian <147114444+emmamelkumian@users.noreply.github.com>
2025-01-30 22:01:15 -0800

    Co-authored-by: S-Shahla <S-Shahla@users.noreply.github.com> added flex capability  to main page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,21 +3,25 @@ import { createRoot } from "react-dom/client";
 import Settings from "./components/settings/settings";
 import SideBar from "./components/sidebar/sidebar";
 import "./index.css";
+import "./preflight.css";
 
 
 const Main = () => {
-  const [showSettings, setShowSettings] = useState(false); 
+  const [showPage, setShowPage] = useState(false); 
 
   // Function to handle the settings click
   const handleSettingsClick = () => {
-    setShowSettings(!showSettings);
+    setShowPage(!showPage);
   };
-
+  const divClassName = `settings ${showPage ? 'show' : ''}`
   return (
     <>
-      <SideBar onSettingsClick={handleSettingsClick} />
-      <div className={`settings ${showSettings ? 'show' : ''}`}>
-        <Settings />
+      <div className="page">
+        <SideBar onSettingsClick={handleSettingsClick} />
+        <div className={divClassName}>
+          {/* Interchange settings/dashboard/inbox Until we make global variables */}
+          <Settings />
+        </div>
       </div>
     </>
   );
