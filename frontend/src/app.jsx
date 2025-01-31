6b0708f0fef18cbab311e3acd4049608c5f04414
S-Shahla <143126171+S-Shahla@users.noreply.github.com>
2025-01-30 22:43:13 -0800

    Rename And Move Main Component
    Renamed main component to Page and moved it to its own file to avoid
    root conflict.

---
+++ frontend/src/app.jsx
@@ -0,0 +1,25 @@
+import {useState} from "react";
+import Settings from "./components/settings/settings";
+import SideBar from "./components/sidebar/sidebar";
+import "./app.css";
+
+export default function Display() {
+  const [showPage, setShowPage] = useState(false);
+
+  // Function to handle the settings click
+  const handleSettingsClick = () => {
+    setShowPage(!showPage);
+  };
+  const divClassName = `settings ${showPage ? "show" : ""}`;
+  return (
+    <>
+      <div className="page">
+        <SideBar onSettingsClick={handleSettingsClick} />
+        <div className={divClassName}>
+          {/* Interchange settings/dashboard/inbox Until we make global variables */}
+          <Settings />
+        </div>
+      </div>
+    </>
+  );
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,35 +1,10 @@
-import { StrictMode, useState } from "react";
-import { createRoot } from "react-dom/client";
-import Settings from "./components/settings/settings";
-import SideBar from "./components/sidebar/sidebar";
-import "./index.css";
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import Display from "./app";
 import "./preflight.css";
-
-
-const Main = () => {
-  const [showPage, setShowPage] = useState(false); 
-
-  // Function to handle the settings click
-  const handleSettingsClick = () => {
-    setShowPage(!showPage);
-  };
-  const divClassName = `settings ${showPage ? 'show' : ''}`
-  return (
-    <>
-      <div className="page">
-        <SideBar onSettingsClick={handleSettingsClick} />
-        <div className={divClassName}>
-          {/* Interchange settings/dashboard/inbox Until we make global variables */}
-          <Settings />
-        </div>
-      </div>
-    </>
-  );
-};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Main/>
+    <Display />
   </StrictMode>
 );
-
