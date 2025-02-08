77d18a4e4f4bb760a048e258324cde1c95c2e913
emmamelkumian <147114444+emmamelkumian@users.noreply.github.com>
2025-02-07 19:41:39 -0800

    using test instead of it

--- frontend/src/tests/settings.test.jsx
+++ frontend/src/tests/settings.test.jsx
@@ -1,28 +1,28 @@
 import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { EmailFetchInterval, Settings, SummariesInInbox, Theme } from '../components/settings/settings';
 // Unit Tests: Test individual functions, components, and utilities.
 // Integration Tests: Test component interactions(e.g., props, state changes).
 // End - to - End Tests: Ensure the app works as expected across user flows.
 describe('Settings Components', () => {
 
-    // Test the Settings components making sure they render
-    it('renders the Settings component', () => {
+    // Rendering the components 
+    test('renders the Settings component', () => {
         render(<Settings />);
         expect(screen.getByText('Settings')).toBeInTheDocument();
     });
 
-    it('renders the SummariesInInbox component', () => {
+    test('renders the SummariesInInbox component', () => {
         render(<SummariesInInbox />);
         expect(screen.getByText('Summaries in Inbox')).toBeInTheDocument();
     });
 
-    it('renders the EmailFetchInterval component', () => {
+    test('renders the EmailFetchInterval component', () => {
         render(<EmailFetchInterval />);
         expect(screen.getByText('Email Fetch Interval')).toBeInTheDocument();
     });
 
-    it('renders the Theme component', () => {
+    test('renders the Theme component', () => {
         render(<Theme />);
         expect(screen.getByText('Theme')).toBeInTheDocument();
     });
