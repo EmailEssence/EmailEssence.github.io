198d2be791edcdcb4d10d918c4c74c5dfe36239c
S-Shahla <143126171+S-Shahla@users.noreply.github.com>
2025-02-07 21:12:10 -0800

    Add Dashboard Component Structure

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -3,8 +3,29 @@ import "./dashboard.css";
 export default function Dashboard() {
   return (
     <div className="dashboard">
-      <h1>DashBoard</h1>
-      <p>Welcome to the DashBoard</p>
+      <WeightedEmailList />
+      <MiniViewPanel />
     </div>
   );
+}
+
+function WeightedEmailList() {
+  return <div></div>;
+}
+
+function MiniViewPanel() {
+  return (
+    <div className="mini-view">
+      <MiniViewHead />
+      <MiniViewBody />
+    </div>
+  );
+}
+
+function MiniViewHead() {
+  return <div></div>;
+}
+
+function MiniViewBody() {
+  return <div></div>;
 }
