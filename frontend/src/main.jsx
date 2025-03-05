fc0ddf74a8aeddb735e574cfa66ff6d3dd83432e
S-Shahla <143126171+S-Shahla@users.noreply.github.com>
2025-03-05 15:25:15 -0800

    Merge CSS Files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./preflight.css";
+import "./main.css";
 import Display from "./page";
 
 createRoot(document.getElementById("root")).render(
