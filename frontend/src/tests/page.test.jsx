023213c083b13f30574d20790b92485ac01f1dc8
S-Shahla <143126171+S-Shahla@users.noreply.github.com>
2025-03-29 11:17:17 -0700

    Split Page Test Into Logged In & Not Logged In

--- frontend/src/tests/page.test.jsx
+++ frontend/src/tests/page.test.jsx
@@ -2,11 +2,16 @@ import { render, screen } from "@testing-library/react";
 import { describe, it, expect, beforeEach } from "vitest";
 import Page from "../page";
 
-describe("Page Component", () => {
-  beforeEach(() => {});
-
-  it("renders login with google button", () => {
+describe("Page Component Not Logged In", () => {
+  it("renders logged out state", () => {
     render(<Page />);
     expect(screen.getByText("Login with Google")).toBeInTheDocument();
+  });
+});
+
+describe("Page Component Logged In", () => {
+  beforeEach(() => {
+    localStorage.clear();
+    localStorage.setItem("auth_token", "mock_token");
   });
 });
