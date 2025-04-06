86dcadfaea3b098f3a00269502649f0f68ad0d05
S-Shahla <143126171+S-Shahla@users.noreply.github.com>
2025-04-05 17:54:33 -0700

    Add Routes Template

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -6,6 +6,15 @@ function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Page />} />
+        <Route element={<Auth />}>
+          <Route path="login" element={<Login />} />
+          <Route path="loading" element={<Loading />} />
+        </Route>
+        <Route path="client">
+          <Route index element={<Dashboard />} />
+          <Route path="inbox" element={<Inbox />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
