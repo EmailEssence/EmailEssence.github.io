b226c8fa1193ff1bb9ab26223105b2f5ba8bdfcb
emmamelkumian <147114444+emmamelkumian@users.noreply.github.com>
2025-04-12 23:26:23 -0700

    initialize error page

--- frontend/src/Error.jsx
+++ frontend/src/Error.jsx
@@ -1,5 +1,29 @@
+import styles from "./Error.module.css";
+
 function Error() {
-  return <></>;
+  const errorMessage = localStorage.getItem("error_message") || "Unknown error";
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.formBox}>
+        <div className={styles.errorDiv}>
+          <div className={styles.errorIcon}>
+            <img src="./src/assets/oldAssets/Logo.svg" alt="error Icon" />
+          </div>
+          <h1 className={styles.errorTitle}>Authentication Error</h1>
+          <p className={styles.errorDescription}>
+            Something went wrong, please try to login again.
+          </p>
+          <p className={styles.errorInfo}>Error: {errorMessage}</p>
+          <button
+            className={styles.retryButton}
+            onClick={() => (window.location.href = "/login")}
+          > Retry Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Error;
