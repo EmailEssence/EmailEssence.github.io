4ef73a2165e0e4080ae5a21d02361805a57f4192
S-Shahla <143126171+S-Shahla@users.noreply.github.com>
2025-04-20 22:35:17 -0700

    Fix Sidebar Tests Not Complying With Router

--- frontend/src/tests/sidebar.test.jsx
+++ frontend/src/tests/sidebar.test.jsx
@@ -1,12 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
-import Sidebar from "../components/sidebar/sidebar";
+import { MemoryRouter } from "react-router";
+import Sidebar from "../components/client/sidebar/sidebar";
 
 describe("Sidebar Component", () => {
   it("Renders Component", () => {
-    render(<Sidebar />);
+    render(
+      <MemoryRouter>
+        <Sidebar />
+      </MemoryRouter>
+    );
     expect(screen.getByTestId("sidebar")).toBeInTheDocument();
   });
-
   // Sidebar Expanding Test In Client
 });
