59b572631010ac89693e9c0b5a1bc04b80e7cbd0
S-Shahla <143126171+S-Shahla@users.noreply.github.com>
2025-04-30 22:47:42 -0700

    Fix main Dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
-import Display from "./Router";
+import Display from "./components/router/Router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
