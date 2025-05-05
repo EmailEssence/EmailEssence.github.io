88d15b239dffe91d12eae74553581400cc4ee797
S-Shahla <143126171+S-Shahla@users.noreply.github.com>
2025-05-04 22:31:51 -0700

    Add Structure For Homepage

--- frontend/src/components/login/Home.jsx
+++ frontend/src/components/login/Home.jsx
@@ -1,5 +1,18 @@
 import "./Home.css";
+import { useNavigate } from "react-router";
 
 export default function Home() {
-  return <div></div>;
+  const navigate = useNavigate();
+  return (
+    <div className="home">
+      <div className="container">
+        <div className="logo"></div>
+        <div className="about"></div>
+        <div
+          className="login-navigate-button"
+          onClick={() => navigate("/login")}
+        ></div>
+      </div>
+    </div>
+  );
 }
