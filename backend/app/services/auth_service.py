37717e3cafc507b610ac90cc8b5b2f8630d58871
Joseph Madigan <jmad1112x@gmail.com>
2025-05-06 12:58:58 -0700

    Auth Scope reorder

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -34,10 +34,10 @@ logger = logging.getLogger(__name__)
 settings = get_settings()
 
 SCOPES = [
-    'https://mail.google.com/',
+    'https://www.googleapis.com/auth/userinfo.profile',
     'https://www.googleapis.com/auth/userinfo.email',
     'openid',
-    'https://www.googleapis.com/auth/userinfo.profile'
+    'https://mail.google.com/'
 ]
 
 class AuthService:
