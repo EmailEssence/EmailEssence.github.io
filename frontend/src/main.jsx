de5d7f5ddc9b712eb3ca1cfb17c14bd87bd0cdc0
S-Shahla <143126171+S-Shahla@users.noreply.github.com>
2025-05-07 15:16:50 -0700

    Fix Base URL In Main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import "./main.css";
 import Display from "./components/router/Router";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={`/${import.meta.env.PUBLIC_URL}`}>
+  <BrowserRouter basename={`/${import.meta.env.BASE_URL}`}>
     <Display />
   </BrowserRouter>
 );
