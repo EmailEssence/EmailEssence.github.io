c87621b8cc85e7d7a994e365646c8f1b5966135c
emmamelkumian <147114444+emmamelkumian@users.noreply.github.com>
2025-06-13 21:53:39 -0700

    comments for loading

--- frontend/src/components/login/Loading.jsx
+++ frontend/src/components/login/Loading.jsx
@@ -3,19 +3,23 @@ import { useNavigate } from "react-router";
 import { handleOAuthCallback } from "../../authentication/authenticate";
 import "./Loading.css";
 
+/**
+ * A loading component that handles the OAuth callback and navigates to the home page.
+ * It displays a loading spinner and text while the OAuth process is completed.
+ */
 export default function Loading() {
   const navigate = useNavigate();
   useEffect(() => {
     const completeOAuth = async () => {
       try {
-        //calls Oauth and updates the loading state
+        /* calls Oauth and updates the loading state */
         await handleOAuthCallback();
 
         navigate("/client/home#newEmails");
-        // Link to /client & mention new emails
+        /* Link to /client & mention new emails */
       } catch (error) {
         console.error("OAuth callback failed:", error);
-        // Optionally navigate to an error page
+        /* Optionally navigate to an error page */
         navigate("/error");
       }
     };
